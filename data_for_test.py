4dd083443dfea40cd8d4eaccb1cf1425866a6120
PerevalovG <gperevaloff@yandex.ru>
2024-11-28 21:49:24 +0500

    учел замечания, и добавил отчет

---
+++ data_for_test.py
@@ -0,0 +1,9 @@
+class Data:
+    name_bun = 'тестовая булка'
+    price_bun = 150
+    name_ingredient_1 = 'тестовый ингридиент 1'
+    price_ingredient_1 = 150
+    type_ingredient_1 = 'SAUCE'
+    name_ingredient_2 = 'тестовый ингридиент 2'
+    price_ingredient_2 = 250
+    type_ingredient_2 = 'FILLING'
